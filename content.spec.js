6450a1e6d030a868b91ac5e05470d8af04e55c61
foo <foo@bar.de>
2020-06-08 10:49:54 +0200

    chore: add some test urls

---
+++ content.spec.js
@@ -0,0 +1,7 @@
+// Websites
+// * https://www.accessibilityoz.com/products/screen-reader-view-firefox-extension/
+// * https://en.wikipedia.org/wiki/World_Wide_Web
+// * https://www.npmjs.com/
+// * https://www.google.de/
+// * https://www.facebook.com/
+// * https://twitter.com/home
